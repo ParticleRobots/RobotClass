0129a6d8fbdd99602e86ade122f5e05515c0676f
lamateos123 <lamateosmit@gmail.com>
2025-05-29 20:35:41 -0700

    Update LuJoystickLightDemmer.ino

--- LU/LuJoystickLightDemmer/LuJoystickLightDemmer.ino
+++ LU/LuJoystickLightDemmer/LuJoystickLightDemmer.ino
@@ -5,8 +5,8 @@ int Button_value=0;
 int UpDown_value=0;
 int LeftRight_value=0;
 
-int BlueLED=A8;
-int RedLED=A9;
+int BlueLED=9;//A8;
+int RedLED=10;//A9;
 
 
 void setup() {
@@ -24,10 +24,11 @@ Serial.begin(9600);
 void loop() {
   // put your main code here, to run repeatedly:
 Button_value=digitalRead(Button);
+UpDown_value=analogRead(UpDown);
 Serial.println(UpDown_value=analogRead(UpDown));
 LeftRight_value=analogRead(LeftRight);
 
-if(UpDown_value>100){
+/*if(UpDown_value>100){
   digitalWrite(RedLED,HIGH);
 }
 else{
@@ -38,7 +39,18 @@ if(LeftRight_value>100){
 }
 else{
   digitalWrite(BlueLED,LOW);
-}
+}*/
+
+int dimmer_red=map(UpDown_value,0,1023,0,255);
+Serial.print("Dimmer red");
+Serial.println(dimmer_red);
+analogWrite(RedLED,dimmer_red);
+
+int dimmer_blue=map(LeftRight_value,0,1023,0,255);
+Serial.print("Dimmer Blue");
+Serial.println(dimmer_blue);
+analogWrite(BlueLED,dimmer_blue);
+
 }
 
 
